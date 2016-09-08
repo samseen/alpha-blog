38da7e495ec56c4de02e27968e47f73cacd5c4cd
samseen <samson.akanbi@outlook.com>
2016-09-08 07:37:11 +0000

    Add edit update functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :articles
 
   get 'signup', to: 'users#new'
-  resources :users, except: [:new]
+  #resources :users, except: [:new]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
