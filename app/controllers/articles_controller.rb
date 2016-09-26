ae7ea62d63847fa43d1894297aa5f77ab076eed9
samseen <samson.akanbi@outlook.com>
2016-09-26 07:20:07 +0000

    Starting out the test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -53,9 +53,9 @@ class ArticlesController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "You can only edit or delete your own articles"
-        redirect_to root_path
+        redirect_to root_path      
       end
     end
 
